18fd93f631c9f5fa125c4ac44a36794735713e3d
seunghyukshin <auseunghyuk@gmail.com>
2023-08-10 12:14:04 +0900

    StringtTest Completed

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -16,9 +16,10 @@ public class StringTest {
         String expected = "adc";
         Assertions.assertEquals(actual, expected);
     }
+
     @Test
     @DisplayName("요구사항1")
-    void split(){
+    void split() {
         // 요구사항 1
         String input = "1,2";
         String[] actual = input.split(",");
@@ -28,25 +29,23 @@ public class StringTest {
 
     @Test
     @DisplayName("요구사항2")
-    void substring(){
+    void substring() {
         String input = "(1,2)";
-        String actual = input.substring(1,input.length() - 1);
+        String actual = input.substring(1, input.length() - 1);
         String expected = "1,2";
         assertThat(actual).isEqualTo(expected);
     }
 
     @Test
     @DisplayName("특정 위치 문자 조회")
-    void charAt(){
+    void charAt() {
         String testData = "abc";
 
         assertThatThrownBy(() -> {
-            String actual = String.valueOf(testData.charAt(1));
-            String expected = "b";
+            String actual = String.valueOf(testData.charAt(3));
 
-            assertThat(actual).isEqualTo(expected);
         }).isInstanceOf(StringIndexOutOfBoundsException.class)
-                .hasMessageContaining("Index: 2, Size: 2");
+                .hasMessageContaining("String index out of range");
 
     }
 }
