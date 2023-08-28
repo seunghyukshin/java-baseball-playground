291f686cac94baf668f0956b90f860030eaea560
seunghyukshin <auseunghyuk@gmail.com>
2023-08-28 12:53:49 +0900

    refactor creating Score.java

---
+++ src/main/java/Score.java
@@ -0,0 +1,30 @@
+public class Score {
+    private int strikeCount;
+    private int ballCount;
+
+    public Score() {
+        this.strikeCount = 0;
+        this.ballCount = 0;
+    }
+
+    public void strike() {
+        this.strikeCount++;
+    }
+
+    public void ball() {
+        this.ballCount++;
+    }
+
+    public boolean isAllStrike() {
+        return strikeCount == 3;
+    }
+
+    @Override
+    public String toString() {
+        if (strikeCount == 0 && ballCount == 0) {
+            return "Nothing";
+        }
+
+        return strikeCount + "스트라이크 " + ballCount + "볼";
+    }
+}
