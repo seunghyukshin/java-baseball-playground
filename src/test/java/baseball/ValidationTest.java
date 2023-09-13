19c0d376a9bc161d4cfc7c25700467fdb5d6a87b
seunghyukshin <auseunghyuk@gmail.com>
2023-09-13 23:08:55 +0900

    feature BallNumberOutOfBoundsException

--- src/test/java/baseball/ValidationTest.java
+++ src/test/java/baseball/ValidationTest.java
@@ -1,13 +1,21 @@
 package baseball;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class ValidationTest {
     @Test
-    public void containsSameValueBallTest(){
-        Balls balls = new Balls();
-//        balls.initRanomBalls();
+    public void isSingleNaturalNumber() throws BallNumberOutOfBoundsException {
 
+        assertThatThrownBy(() -> {
+            Ball ball = new Ball( 3,17);
+        }).isInstanceOf(BallNumberOutOfBoundsException.class);
+
+
+//        .hasMessageContaining("String index out of range");
 
     }
 }
